cf745c4d735c7c2fe4c2557ad7396b41bf273a2c
katopz <katopz@gmail.com>
2020-06-04 17:29:37 +0700

    Updates

---
+++ precache-manifest.375a4317083689cd784e1def55d63e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f119b6f3261c19838b6034ff6bf4ea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c32235d95d15654053",
+    "url": "/static/css/main.55695ef4.chunk.css"
+  },
+  {
+    "revision": "745ac998d7dd576d9335",
+    "url": "/static/js/2.35187237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35187237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c32235d95d15654053",
+    "url": "/static/js/main.2ba818bd.chunk.js"
+  },
+  {
+    "revision": "1e978a6d9678d4b6fd09",
+    "url": "/static/js/runtime-main.8840093a.js"
+  }
+]);
